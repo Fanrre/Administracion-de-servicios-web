b570f2ba3209955d41cbdf7eec433c8ba850c04d
Fanrre <120412628+Fanrre@users.noreply.github.com>
2022-12-12 15:53:48 -0300

    Add files via upload

---
+++ patricio.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<head>
+     <title>Taller Evualado De Administracion Web </title>
+
+</head>
+<body
+    bgcolor="white"
+    text="black">
+<center>
+
+<?php
+class MyClass {
+      public static $PDJ = "<h3>Soy Patricio De La Jara";
+      public static $PA  = "Ramo: Administracion De Servicio Web <br>";
+
+
+}
+echo Myclass::$PDJ."<br>";
+echo Myclass::$PA; "<br>";
+echo "La fecha de hoy es " . date("d/m/y") . "<br>";
+date_default_timezone_set("America/Santiago");
+echo "La hora es: " .date("G:i.s");
+?>
+
+</center>
+</body>
+</html>
